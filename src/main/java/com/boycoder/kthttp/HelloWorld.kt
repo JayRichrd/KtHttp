a7e6ebc0813bda89db6f0065d10b60881bd9b344
YuJiang <jiangfengfn12@163.com>
2022-08-12 13:30:21 +0800

    feat(log4j): add log4j for log

--- src/main/java/com/boycoder/kthttp/HelloWorld.kt
+++ src/main/java/com/boycoder/kthttp/HelloWorld.kt
@@ -1,5 +1,19 @@
 package com.boycoder.kthttp
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+val logger: Logger = LogManager.getLogger("HelloWord")
 fun main() {
-    println("Hello world.")
+    GlobalScope.launch(Dispatchers.IO) {
+        delay(1000L)
+        logger.debug("Coroutine started:${Thread.currentThread().name}")
+    }
+
+    logger.debug("After launch:${Thread.currentThread().name}")
+    Thread.sleep(2000L)
 }
